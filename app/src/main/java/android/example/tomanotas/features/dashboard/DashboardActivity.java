b5b9c4d4fda4bca6f13957e64174ee815ff07b61
david.abellan <david.abellan@movetia.com>
2019-10-04 13:23:55 +0200

    Refactor - renombro de main_activity a dashboard_activity. Organizo por features.

--- app/src/main/java/android/example/tomanotas/features/dashboard/DashboardActivity.java
+++ app/src/main/java/android/example/tomanotas/features/dashboard/DashboardActivity.java
@@ -1,14 +1,15 @@
-package android.example.tomanotas;
+package android.example.tomanotas.features.dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.example.tomanotas.R;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dashboard);
     }
 }
